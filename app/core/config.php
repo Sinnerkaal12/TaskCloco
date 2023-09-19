2854ee8ca8e6946e0d012e8254507fcb2a7df6f4
sushantmalla1245 <sushantmalla@mavorion.com>
2023-09-19 20:52:43 +0545

    Added dashboard and user pages

--- app/core/config.php
+++ app/core/config.php
@@ -4,22 +4,22 @@ if($_SERVER['SERVER_NAME'] == "localhost")
 {
 
 	//for local server
-	define("ROOT", "http://localhost/music_website/public");
+	define("ROOT", "http://localhost/musicapp/public");
 
 	define("DBDRIVER", "mysql");
 	define("DBHOST", "localhost");
 	define("DBUSER", "root");
 	define("DBPASS", "");
-	define("DBNAME", "music_website_db");
+	define("DBNAME", "musicapp");
 
 }else{
 
 	//for online server
-	define("ROOT", "http://www.mywebsite.com");	
+	define("ROOT", "");	
 
 	define("DBDRIVER", "mysql");
 	define("DBHOST", "localhost");
 	define("DBUSER", "root");
 	define("DBPASS", "");
-	define("DBNAME", "music_website_db");
+	define("DBNAME", "musicapp");
 }
--- app/pages/includes/admin-header.php
+++ app/pages/includes/admin-header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Music Website</title>
+	<title>Musical Website</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/style.css?52423">
